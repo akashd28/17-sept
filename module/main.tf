9ab3b8e5c45798ae15e30dcbcfb7767a4a0da81f
akashdsai <dsaiakaash@gmail.com>
2024-09-20 09:00:01 +0530

    updates

--- module/main.tf
+++ module/main.tf
@@ -1,7 +1,6 @@
 module "s3" {
   source      = "./s3"
-  for_each    = var.bucket
-  bukcet_name = var.bucket_name
+  bucket_name = var.bucket_name
   depends_on  = [module.vpc]
 }
 
--- module/terraform.tfvars
+++ module/terraform.tfvars
@@ -1,3 +1,3 @@
-bucker_name =  "akash"
+bucket_name =  "akash34567"
 
 cidr  ="10.0.0.0/16"
